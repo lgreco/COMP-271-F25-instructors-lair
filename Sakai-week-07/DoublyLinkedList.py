d37f8eea1bda0516eac1a02884f17492ab368516
Leo Irakliotis <lgreco@gmail.com>
2025-09-29 12:52:54 +0000

    has_loop

--- Sakai-week-07/DoublyLinkedList.py
+++ Sakai-week-07/DoublyLinkedList.py
@@ -3,43 +3,43 @@ from Node import Node
 class DoublyLinkedList:
 
     def __init__(self):
-        self.head = None
-        self.tail = None
-        self.size = 0
+        self.__head = None
+        self.__tail = None
+        self.__size = 0
 
     def __str__(self):
         return self.__repr__()
 
     def __repr__(self):
         nodes = []
-        current = self.head
+        current = self.__head
         while current is not None:
             nodes.append(str(current))
             current = current.get_next()
         return " <-> ".join(nodes)
 
     def is_empty(self):
-        return self.size == 0
+        return self.__size == 0
 
     def get_size(self):
-        return self.size
+        return self.__size
 
     def add_to_back(self, data):
         new_node = Node(data)
         if self.is_empty():
-            self.head = new_node
+            self.__head = new_node
         else:
-            new_node.set_prev(self.tail)
-            self.tail.set_next(new_node)
-        self.tail = new_node
-        self.size += 1
+            new_node.set_prev(self.__tail)
+            self.__tail.set_next(new_node)
+        self.__tail = new_node
+        self.__size += 1
     
     def add_to_front(self, data):
         new_node = Node(data)
         if self.is_empty():
-            self.tail = new_node
+            self.__tail = new_node
         else:
-            new_node.set_next(self.head)
-            self.head.set_prev(new_node)
-        self.head = new_node
-        self.size += 1
+            new_node.set_next(self.__head)
+            self.__head.set_prev(new_node)
+        self.__head = new_node
+        self.__size += 1
--- Sakai-week-07/SOLUTIONS_DoublyLinkedList copy.py
+++ Sakai-week-07/SOLUTIONS_DoublyLinkedList copy.py
@@ -4,46 +4,46 @@ from Node import Node
 class DoublyLinkedList:
 
     def __init__(self):
-        self.head = None
-        self.tail = None
-        self.size = 0
+        self.__head = None
+        self.__tail = None
+        self.__size = 0
 
     def __str__(self):
         return self.__repr__()
 
     def __repr__(self):
         nodes = []
-        current = self.head
+        current = self.__head
         while current is not None:
             nodes.append(str(current))
             current = current.get_next()
         return " <-> ".join(nodes)
 
     def is_empty(self):
-        return self.size == 0
+        return self.__size == 0
 
     def get_size(self):
-        return self.size
+        return self.__size
 
     def add_to_back(self, data):
         new_node = Node(data)
         if self.is_empty():
-            self.head = new_node
+            self.__head = new_node
         else:
-            new_node.set_prev(self.tail)
-            self.tail.set_next(new_node)
-        self.tail = new_node
-        self.size += 1
+            new_node.set_prev(self.__tail)
+            self.__tail.set_next(new_node)
+        self.__tail = new_node
+        self.__size += 1
 
     def add_to_front(self, data):
         new_node = Node(data)
         if self.is_empty():
-            self.tail = new_node
+            self.__tail = new_node
         else:
-            new_node.set_next(self.head)
-            self.head.set_prev(new_node)
-        self.head = new_node
-        self.size += 1
+            new_node.set_next(self.__head)
+            self.__head.set_prev(new_node)
+        self.__head = new_node
+        self.__size += 1
 
     def find_middle(self):
         """Returns the middle node of the list. If the list has an even number of nodes,
@@ -54,8 +54,8 @@ class DoublyLinkedList:
         # Check if the list is empty, and if so return the None object
         if not self.is_empty():
             # Set two pointers, one starting at the head and the other at the tail
-            forward = self.head
-            backward = self.tail
+            forward = self.__head
+            backward = self.__tail
             # Move the pointers toward each other until they meet or cross
             while forward != backward and forward.get_next() != backward:
                 forward = forward.get_next()
@@ -65,13 +65,9 @@ class DoublyLinkedList:
 
     def has_loop(self) -> bool:
         """Returns True if the list has a loop, False otherwise."""
-        loop = False
-        if not self.is_empty():
-            slow = self.head
-            fast = self.head
-            while fast.has_next() and fast.get_next().has_next() and not loop:
-                slow = slow.get_next()
-                fast = fast.get_next().get_next()
-                loop = slow == fast
-        return loop
-    
+        return (
+            self.__head is not None
+            and self.__tail is not None
+            and self.__head.get_prev() == self.__head
+            and self.__tail.get_next() == self.__tail
+        )
